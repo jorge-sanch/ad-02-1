88b8fbc6d6fdfd9662ec378d7720a56d23a9e747
Jorge Sanchez <sanchez.j.jolu@gmail.com>
2026-01-14 13:19:48 -0600

    Se realizo la actividad correctamente

---
+++ script.js
@@ -0,0 +1,5 @@
+const h3Element = document.getElementById("cambioColor")
+h3Element.style.color = "orange";
+h3Element.addEventListener("click", (event) =>{
+    h3Element.style.color = "rgba(216, 91, 91, 0.836)";
+})
